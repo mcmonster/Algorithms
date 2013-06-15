e17250ff8d106bc08609c540b3d15891d4e0e8b2
R. Matt McCann <mccann.matt@gmail.com>
2013-06-15 01:21:15 -0400

    Implemented merge sort, quick sort, and bubble sort

--- python/src/Sort.py
+++ python/src/Sort.py
@@ -1,9 +1,22 @@
 '''
+Sorts the provided items using the bubble sort algorithm
+'''
+def bubbleSort(items):
+    for itemIter in range(0, len(items)): # For each item in the list
+        stride = -1
+        
+        for reverseIter in range(len(items) - 1, itemIter, stride): # Search in reverse
+            # If the current item is smaller than the item before it
+            if items[reverseIter] < items[reverseIter - 1]:
+                # Swap it with the item before it
+                items[reverseIter], items[reverseIter - 1] = items[reverseIter - 1], items[reverseIter]
+
+'''
 Builds a legal heap by iteratively applying heapify.
 
 Runtime: O(n)
 '''
-def __buildHeap(items):
+def _buildHeap(items):
     print "__buildHeap()..."
     print "\titems: ", items
     
@@ -12,27 +25,37 @@ def __buildHeap(items):
     print "\tRootNode: ", rootNode
     
     # For each node starting with the first non-leaf node up to and including the root
-    print "\tRange(", firstNonLeafNode, ",", rootNode - 1, ")"
+    print "\tRange[", firstNonLeafNode, ",", rootNode - 1, ")"
     step = -1
     for nodeIter in range(firstNonLeafNode, rootNode - 1, step):
         # Make the current node's 
-        __heapify(items, nodeIter, len(items))
+        _heapify(items, nodeIter, len(items))
     
 '''
 Sorts a given node into the correct position by recursively sifting it down the heap.
 
 Runtime: O(lg n)
 '''
-def __heapify(items, pos, heapSize):
+def _heapify(items, pos, heapSize):
     print "__heapify()..."
     print "\titems: ", items
-    
+    print "\theapSize: ", heapSize    
+
     leftChild = pos * 2 + 1
     rightChild = pos * 2 + 2
     largestNode = pos
-    print "\tRoot: ", pos
-    print "\tLeftChild: ", leftChild
-    print "\tRightChild: ", rightChild
+    print "\tRoot Pos: ", pos
+    print "\tRoot Value: ", items[pos]
+    print "\tLeftChild Pos: ", leftChild
+    if leftChild < heapSize:
+        print "\tLeftChild Value: ", items[leftChild]
+    else:
+        print "\tLeftChild Value: N/A"
+    print "\tRightChild Pos: ", rightChild
+    if rightChild < heapSize:
+        print "\tRightChild Value: ", items[rightChild]
+    else:
+        print "\tRightChild Value: N/A"
     
     # If the left child exists and is greater than the parent node
     if (leftChild < heapSize) and (items[leftChild] > items[largestNode]):
@@ -42,15 +65,17 @@ def __heapify(items, pos, heapSize):
     if (rightChild < heapSize) and (items[rightChild] > items[largestNode]):
         largestNode = rightChild # Save it as the largest node
         
-    print "\tLargestNode: ", largestNode
+    print "\tLargestNodePos: ", largestNode
+    print "\tLargestNodeValue: ", items[largestNode]
         
     # If at least one of the children nodes is larger than the parent node
     if largestNode != pos:
         # Swap the parent node with its larger child
         items[pos], items[largestNode] = items[largestNode], items[pos]
-        
+        print "\tItems after swap: ", items        
+
         # Continue sifting the node down the heap
-        __heapify(items, largestNode, heapSize)
+        _heapify(items, largestNode, heapSize)
 
 '''
 Sorts the provided items using the heap sort algorithm
@@ -59,23 +84,27 @@ Runtime: O(n lg n)
 '''
 def heapSort(items):
     # Initialize the items into a legal, but unsorted, heap
-    __buildHeap(items)
-    
+    _buildHeap(items)
+    print "Done building heap: ", items
+ 
     heapSize = len(items)
     
-    # Starting with the last node and working up to the root of the heap 
+    # Starting with the last node and working up to the root of the heap
+    print "Range: ", len(items) - 1, ",", 0 
     step = -1
-    for nodeIter in range(len(items) - 1, step):
+    for nodeIter in range(len(items) - 1, 0, step):
+        print "Items: ", items
         rootPos = 0
         
         # Swap the current root with the last node in the heap
-        items[rootPos], items[nodeIter] = items[rootPos], items[nodeIter]
-        
+        items[rootPos], items[nodeIter] = items[nodeIter], items[rootPos]
+        print "Items After Swap: ", items
+
         # Mark the node now last in the heap as correctly sorted
         heapSize = heapSize - 1
         
         # Sort the new root value in order to maintain the state of being a heap
-        __heapify(items, rootPos, heapSize)
+        _heapify(items, rootPos, heapSize)
         
 
 '''
@@ -103,4 +132,125 @@ def insertionSort(items):
             
         # Insert the current item into the selected location
         items.insert(searchPos + 1, currentItem)
-        
+     
+'''
+Merges the two sub-arrays together in order
+'''
+def _merge(items, subArrayStart, pivot, subArrayEnd):
+    leftSubArrayLength = pivot - subArrayStart + 1 # Determine the lengths of each sub-array
+    rightSubArrayLength = subArrayEnd - pivot
+
+    # Merge sort cannot sort in place so created two sub-array working spaces
+    leftSubArray = [] # Copy the left sub array items into the working space
+    for item in items[subArrayStart:subArrayStart+leftSubArrayLength]:
+        leftSubArray.append(item) # Add it to the working space
+    rightSubArray = [] # Copy the right sub array items into the working space
+    for item in items[pivot+1:pivot+1+rightSubArrayLength]:
+        rightSubArray.append(item) # Add it to the working space
+
+    leftSubArray.append(9999999) # Add sentinel values to the end of each sub-array
+    rightSubArray.append(9999999) # TODO Find type safe way of defining sentinel    
+
+    leftPos = 0 # Initialize the sub-array search positions
+    rightPos = 0
+
+    print "Range: [", subArrayStart, ",", subArrayEnd + 1, ")"
+    for itemIter in range(subArrayStart, subArrayEnd + 1): # For each position in the arrayi
+        print "=========================="
+        print "Items: ", items
+        print "ItemIter: ", itemIter
+        print "Left: ", leftSubArray
+        print "LeftPos: ", leftPos
+        print "Right: ", rightSubArray
+        print "RightPos: ", rightPos
+
+        if leftSubArray[leftPos] <= rightSubArray[rightPos]: # If the next left sub-array item is smaller
+            items[itemIter] = leftSubArray[leftPos] # Add it to the sorted list
+            leftPos = leftPos + 1 # Move to the next left sub-array item
+        else: # If the next righ sub-array item is smaller
+            items[itemIter] = rightSubArray[rightPos] # Add it to the sorted lsit
+            rightPos = rightPos + 1 # Move to the next righ sub-array item
+    print "Final Items: ", items
+
+'''
+Sorts the provided items by recursively merge sorting
+'''
+def _mergeSort(items, subArrayStart, subArrayEnd):
+    if subArrayStart < subArrayEnd: # If the sub-array is not empty
+        pivot = (subArrayStart + subArrayEnd) / 2 # Split the array into two sub-array halfs
+
+        _mergeSort(items, subArrayStart, pivot) # Recursively sort the sub-arrays
+        _mergeSort(items, pivot + 1, subArrayEnd)
+        _merge(items, subArrayStart, pivot, subArrayEnd) # Merge the sub-arrays together
+
+'''
+Sorts the provided items using the merge sort algorithm
+'''
+def mergeSort(items):
+    _mergeSort(items, 0, len(items) - 1)
+ 
+'''
+Sorts the sub array such that all items below the pivot position are
+less than or equal to the item at the pivot position and all values 
+in positions past the pivot position are greater than or equal to the
+pivot value.
+'''
+def _partition(items, subArrayStart, subArrayEnd):
+    # Validate the input parameters
+    assert (0 <= subArrayStart) and (subArrayStart < len(items))
+    assert (subArrayStart <= subArrayEnd) and (subArrayEnd < len(items))
+    
+    print "Items: ", items
+    print "Sub Array Start: ", subArrayStart
+    print "Sub Array End: ", subArrayEnd    
+
+    # Pick the last item in the sub-array as the pivot value
+    pivotValue = items.pop(subArrayEnd)
+    pivotPos = subArrayStart - 1
+    
+    print "Pivot Value: ", pivotValue
+    print "Pivot Pos: ", pivotPos
+
+    # For each item in the sub-array except the pivot value at the end
+    print "Range[", subArrayStart, ",", subArrayEnd, ")"
+    for itemIter in range(subArrayStart, subArrayEnd):
+        currentItem = items[itemIter]
+
+        print "Items: ", items
+        print "Current Item: ", currentItem
+
+        if currentItem <= pivotValue: # If the current item is <= the pivot value
+            pivotPos = pivotPos + 1 # Move the pivot position forward
+
+            currentItem = items.pop(itemIter)
+            items.insert(pivotPos, currentItem)
+
+    # Put the pivot value into the selected pivot position
+    pivotPos = pivotPos + 1
+    items.insert(pivotPos, pivotValue)
+    
+    print "Items: ", items
+
+    return pivotPos
+
+'''
+Internal recursive quick sort implementation
+'''
+def _quickSort(items, subArrayStart, subArrayEnd):
+    assert subArrayStart >= 0 # Validate the input parameters
+    assert subArrayEnd < len(items) 
+
+    if subArrayStart < subArrayEnd: # If the sub-array to be sorted is not empty
+        # Calculate the pivot point along which to split the sub-array
+        pivotPos = _partition(items, subArrayStart, subArrayEnd)
+
+        # Recursively quick-sort the two sub-array
+        _quickSort(items, subArrayStart, pivotPos - 1)
+        _quickSort(items, pivotPos + 1, subArrayEnd)    
+
+'''
+Sorts the provided items using the quicksort algorithm
+'''
+def quickSort(items):
+    _quickSort(items, 0, len(items) - 1)
+
--- python/src/SortTest.py
+++ python/src/SortTest.py
@@ -2,12 +2,19 @@ import random
 import Sort
 import unittest
 
-class Sortest(unittest.TestCase):
+class SortTest(unittest.TestCase):
     # Before each test
     def setUp(self):
         # Initialize a range of random integers
         self.testData = range(-10, 10)
         random.shuffle(self.testData)
+
+    # Test the bubble sort algorithm
+    def testBubbleSort(self):
+        # Test that the list is sorted
+        data = [1, 5, 9, 1, 3, 2]
+        Sort.bubbleSort(data)
+        self.assertEqual(data, [1, 1, 2, 3, 5, 9])
         
     # Test the insertion sort algorithm
     def testInsertionSort(self):
@@ -17,13 +24,70 @@ class Sortest(unittest.TestCase):
         # Verify that the numbers were properly sorted
         self.assertEqual(self.testData, range(-10, 10))
         
+    # Test the heapify function
+    def testHeapify(self):
+        # Test that the root node is sorted to the left child
+        data = [1, 9, 5]
+        Sort._heapify(data, 0, 3)
+        self.assertEqual(data, [9, 1, 5])
+
+        # Test that the root node is sorted to the bottom left grand-child
+        data = [1, 5, 9, 1, 3, 2]
+        Sort._heapify(data, 0, 6)
+        self.assertEqual(data, [9, 5, 2, 1, 3, 1])
+
+    # Test the buildHeap function
+    def testBuildHeap(self):
+        # Test that the list is built into a heap
+        data = [1, 5, 9, 1, 3, 2]
+        Sort._buildHeap(data)
+        self.assertEqual(data, [9, 5, 2, 1, 3, 1])
+
     # Test the heap sort algorithm
     def testHeapSort(self):
-        # Sort the random numbers
-        Sort.heapSort(self.testData)
-        
-        # Verify that the numbers were properly sorted
-        self.assertEqual(self.testData, range(-10, 10))
-        
+        # Test a small list
+        data = [1, 5, 9, 1, 3, 2]
+        Sort.heapSort(data)
+        self.assertEqual(data, [1, 1, 2, 3, 5, 9])        
+
+    # Test the partition function
+    def testPartition(self):
+        # Test that the list is properly partitioned
+        data = [1, 5, 9, 1, 3, 2]
+        pivotPos = Sort._partition(data, 0, 5)
+        self.assertEqual(data, [1, 1, 2, 5, 9, 3])
+        self.assertEqual(pivotPos, 2)
+
+    # Test the quick sort function
+    def testQuickSort(self):
+        # Test that the list is properly sorted
+        data = [1, 5, 9, 1, 3, 2]
+        Sort.quickSort(data)
+        self.assertEqual(data, [1, 1, 2, 3, 5, 9])
+
+    # Tests the merge function
+    def testMerge(self):
+        # Test that the two sub-arrays created from an even number of items are properly merged
+        data = [1, 9, 2, 2]
+        Sort._merge(data, 0, 1, 3)
+        self.assertEqual(data, [1, 2, 2, 9])
+
+        # Test that the two sub-arrays created from an off number of items are properly merged
+        data = [3, 9, 2, 1, 7]
+        Sort._merge(data, 0, 2, 4)
+        self.assertEqual(data, [1, 3, 7, 9, 2])
+
+    # Test the merge sort function
+    def testMergeSort(self):
+        # Test that the list is properly sorted when using an even number of items
+        data = [1, 5, 9, 1, 3, 2]
+        Sort.mergeSort(data)
+        self.assertEqual(data, [1, 1, 2, 3, 5, 9])
+
+        # Test that the list is properly sorted when using an odd number of items
+        data = [1, 5, 9, 1, 3, 2, 7]
+        Sort.mergeSort(data)
+        self.assertEqual(data, [1, 1, 2, 3, 5, 7, 9])
+
 if __name__ == '__main__':
     unittest.main()
